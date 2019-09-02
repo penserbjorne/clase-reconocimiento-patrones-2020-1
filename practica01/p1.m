3f8bb23797d41d041cfb0f9cdde40c79cde01486
Penserbjorne <paul.aguilar.enriquez@hotmail.com>
2019-09-02 00:12:59 -0500

    Se respaldo el script original y se añadieron cambios de Simon

--- practica01/p1.m
+++ practica01/p1.m
@@ -5,16 +5,27 @@ clc
 im = imread('imagen.png');
 im_g = rgb2gray(im);
 
-filtro=fspecial('gaussian',[5,5]);
-imFilG=imfilter(im_g,filtro);
+filtro5=fspecial('gaussian',[5,5]);
+filtro9=fspecial('gaussian',[9,9]);
+filtro11=fspecial('gaussian',[11,11]);
+imFilG5=imfilter(im_g,filtro5);
+imFilG9=imfilter(im_g,filtro9);
+imFilG11=imfilter(im_g,filtro11);
 imFilM=medfilt2(im_g);
 
 mediaIntensidad=mean(imFilM(:));
 im_bin= imFilM > mediaIntensidad;
 
-figure,
-subplot(1,4,1),imshow(im_g)
-subplot(1,4,2),imshow(imFilG)
-subplot(1,4,3),imshow(imFilM)
-subplot(1,4,4),imshow(im_bin)
+[ren,col]=size(im);
+im_r=imcrop(im_bin,[288,300,90,70]);
+
+figure,title('Filtros')
+subplot(2,4,1),imshow(im),title('Original')
+subplot(2,4,2),imshow(im_g),title('Escala de grises')
+subplot(2,4,3),imshow(imFilG5),title('Gausiano 5X5')
+subplot(2,4,4),imshow(imFilG9),title('Gausiano 9X9')
+subplot(2,4,5),imshow(imFilG11),title('Gausiano 11X11')
+subplot(2,4,6),imshow(imFilM),title('Media no lineal')
+subplot(2,4,7),imshow(im_bin),title('Binaria')
+subplot(2,4,8),imshow(im_r),title('Binaria recortada')
 
